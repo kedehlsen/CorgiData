561b79c344a346e1cf595c61fe8c3f7783a3ef22
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-11-15 09:23:03 -0800

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,24 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/p1") #annotations tell which function goes with which request
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
+
+
+
+if __name__=="__main__":
+
