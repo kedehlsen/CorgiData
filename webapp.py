5d783079a227a337cd1a4b9e7f376361e4814214
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-12-10 09:24:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -25,7 +25,7 @@ def get_state():
         counties = json.load(election)
     state = request.args["state"]
 
-    return render_template("page1.html",  stateoptions=get_state_options(counties), options=get_county_options(counties))
+    return render_template("page1.html",  stateoptions=get_state_options(counties), options=get_county_options(state, counties))
 
 @app.route("/getfact1")
 def getfact1():
