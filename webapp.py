30488e0965c4b8dca4df0fe6058141849ccf8748
Karleigh <kedehlsen@gmail.com>
2019-12-19 10:17:59 -0800

    works!

--- webapp.py
+++ webapp.py
@@ -33,14 +33,21 @@ def getfact1():
             county = request.args["county"]
             render= render_template("page1.html", stateoptions=get_state_options(counties,state), options=get_county_options(county, state, counties), dem_info = get_popular_dem(state, county, counties), rep_info = get_popular_rep(state, county, counties), state_input=state)
         else:
-            render = render_template('page1.html', stateoptions=get_state_options(counties,state),options=get_county_options("", state,counties))
+            render = render_template('page1.html', stateoptions=get_state_options(counties,state),options=get_county_options("", state,counties), state_input=state)
     else:
         render = render_template('page1.html', stateoptions=get_state_options(counties,""))
     return render
         
-@app.route("/p2")
+@app.route("/p2") #THIS IS WHERE YOU ACTUALLY ARE, Make a graph of the amount of total repub votes vs total dem votes YOU can delete the bottom stuff, idk if it works
 def render_page2():
-    return render_template('page2.html')
+    with open('election.json') as election:
+        counties = json.load(election)
+    if "state" in request.args:
+        state = request.args["state"]
+        render = render_template('page1.html', stateoptions=get_state_options(counties,""),options=get_county_options("","",counties))
+    else:
+        render = render_template('page1.html', stateoptions=get_state_options(counties,""))
+    return render
 
 @app.route("/p3")
 def render_page3():
@@ -54,8 +61,8 @@ def get_county_options(county_selected, state_selected, counties):
             if data['Location']['State'] == state_selected:
                 listOfCounties.append(data['Location']['County'])
     for county in listOfCounties:
-        print(county_selected)
-        if (county_selected == "") or not(county['Location']['County'] == county_selected):
+        print(county)
+        if (county_selected == "") or not(county == county_selected):
             options = options + Markup("<option value=\"" + county + "\">" + county + "</option>")
         else:
            options = options + Markup("<option value=\"" + county + "\"" + " selected" + ">" + county + "</option>")
@@ -79,12 +86,10 @@ def get_popular_dem(state,county,counties):
     democrat={}
     
     countyData = {}
-    
     for c in counties:
-        if c["Location"]["State"] == county:
-            if c["Location"]["County"] == state:
+        if c["Location"]["State"] == state:
+            if c["Location"]["County"] == county:
                 countyData = c
-    print(countyData)
     #here is what YOU WERE WORKING ON
     
     for person in countyData["Vote Data"]:
@@ -107,8 +112,8 @@ def get_popular_rep(state,county,counties):
     countyData = {}
     
     for c in counties:
-        if c["Location"]["State"] == county:
-            if c["Location"]["County"] == state:
+        if c["Location"]["State"] == state:
+            if c["Location"]["County"] == county:
                 countyData = c
     
     for person in countyData["Vote Data"]:
